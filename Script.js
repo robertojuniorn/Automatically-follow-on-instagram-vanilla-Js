d283b469b92aaf7a4bec22d1360550afd84e0db1
Roberto Júnior <robertojuniorn@gmail.com>
2021-05-13 23:00:40 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -3,8 +3,11 @@ let contador = 0;
 // Pegamos a class do button seguir atraves do DOM
 let arr = document.querySelectorAll('.L3NKy');
 
-// Pegamos o array e percorre
+// Pegamos o array e usamos o forEach para percorrer
 arr.forEach((v, i) =>{
+
+// Vai executar o bloco de codigo depois depois de 10 segundos
+
     setTimeout(() => {
         if(!v.classList.contains('_8A5w5')){
             console.log('Roberto voce deixou de seguir '+contador+' pessoas ');
