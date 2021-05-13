2f160fd9b43815eafd55dfa64aeab4130126252d
Roberto Júnior <robertojuniorn@gmail.com>
2021-05-13 22:51:32 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,10 +1,12 @@
 
 let contador = 0;
-let arr = document.querySelectorAll('._8A5w5');
+// Pegamos a class do button seguir atraves do DOM
+let arr = document.querySelectorAll('.L3NKy');
 
+// Pegamos o array e percorre
 arr.forEach((v, i) =>{
     setTimeout(() => {
-        if(!v.classList.contains('L3NKy')){
+        if(!v.classList.contains('_8A5w5')){
             console.log('Roberto voce deixou de seguir '+contador+' pessoas ');
         }else{
             console.log('Voce parou de seguir este usuario a bastante tempo.')
